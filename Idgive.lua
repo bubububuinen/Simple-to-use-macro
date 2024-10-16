83d3efbf7e13edbea9cf964cfdfc4ea2ca2b2f2f
BubuinenBubu <144718238+bubububuinen@users.noreply.github.com>
2024-10-16 17:37:25 +0300

    Create Idgive.lua
    This improves the safety of this program MASSIVELY, since now for someone to gain access to your macro will require them needing your Github account.

---
+++ Idgive.lua
@@ -0,0 +1,16 @@
+local ID = UserID
+local Error1 = error1
+local Error2 = error2
+local Error3 = error3
+local Error4 = error4
+local running = macro.zip, ui.zip
+If running = true display ID
+else display Error1
+  wait (2)
+  if ID = 0 then display Error2
+    wait (2)
+    if ID = null then display Error3
+      wait (2)
+      if ID = then display Error4
+        wait (2)
+
