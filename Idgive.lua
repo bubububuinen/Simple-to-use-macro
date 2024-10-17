f2e9add282c62ec4abab1c78714a86e3a1260c86
BubuinenBubu <144718238+bubububuinen@users.noreply.github.com>
2024-10-17 14:46:19 +0300

    Update Idgive.lua
    Added a second layer of protection

--- Idgive.lua
+++ Idgive.lua
@@ -1,9 +1,11 @@
 local ID = UserID
+local SecurityCode = math.random()
+local Verify = SecurityCode, Verify.jpg, VerifyError.json, VerifyComplete.json, VerifyFail.json
 local Error1 = error1
 local Error2 = error2
 local Error3 = error3
 local Error4 = error4
-local running = macro.zip, ui.zip
+local running = macro.zip, ui.zip, Idgive.lua
 If running = true display ID
 else display Error1
   wait (2)
@@ -14,3 +16,8 @@ else display Error1
       if ID = then display Error4
         wait (2)
 
+If SecurityCode = null then display Error3
+If SecurityCode = False then display Error1
+If Verify = False then display Error1
+If Verify = null then display Error3
+If SecurityCode = True then Verify
